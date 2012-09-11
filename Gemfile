4186bf138ad265f96653500409b0a54225b6b442
Michael Whalen <michael@whalesalad.com>
2012-09-10 21:32:45 -0400

    Initial salad on rails!

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'pg'
+gem 'haml'
+gem 'thin'
+
+# Gems used only for assets and not in production
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'jquery-rails'
+end
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'annotate', :git => 'git://github.com/jeremyolliver/annotate_models.git', :branch => 'rake_compatibility'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whalesalad::Application.config.secret_token = 'ba3764e8f70ac39d48fd53da1cd0a94fa49185cdad8a4f42f99433ee630b735a2382d621df843a49dc8ecf20ef60c099ad578eb148eeeac79a0c3a57b7215c68'
